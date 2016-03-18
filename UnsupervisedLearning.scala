62b7b3d1a5d7a8202794bae8af378f4abe9ed785
Chris Hodapp <Hodapp87@gmail.com>
2016-03-18 19:09:00 -0400

    More scratch work with k-means & PCA.

--- UnsupervisedLearning.scala
+++ UnsupervisedLearning.scala
@@ -11,14 +11,16 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.{Vectors, Vector}
+import org.apache.spark.mllib.clustering._
 import org.apache.spark.mllib.feature.PCA
 
-import org.apache.spark.mllib.clustering.GaussianMixture
-import org.apache.spark.mllib.clustering.GaussianMixtureModel
+import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
+
+import breeze.linalg.{DenseVector => BDV}
 
 import org.apache.log4j.{Level, Logger}
+
 
 //import com.github.tototoshi.csv._
 
@@ -28,36 +30,63 @@ object UnsupervisedLearning {
     
     // TODO: Fix the below to work with what's given in
     // https://spark.apache.org/docs/latest/programming-guide.html#initializing-spark
-    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val conf = new SparkConf()
+      .setAppName("Unsupervised Learning")
+      .setMaster("local[*]")
     val sc = new SparkContext(conf)
     // TODO: Can I just log to a file instead?
     val rootLogger = Logger.getRootLogger()
     rootLogger.setLevel(Level.ERROR)
 
     val faults = readSteelFaults(sc)
+    val faultsIn = faults.map(_._1)
+    val faultsOut = faults.map(_._2)
+    val summary: MultivariateStatisticalSummary = Statistics.colStats(faultsIn)
+    println("Mean: %s" format summary.mean)
+    println("Variance: %s" format summary.variance)
 
-    // Attempt PCA
-    val dims = 15
-    val pca = new PCA(dims).fit(faults)
-    val pcaMtx = pca.pc
-    println(s"PCA matrix: $pcaMtx")
-    val faults2 = pca.transform(faults)
+    val mean = new BDV(summary.mean.toArray)
+    val stdev = new BDV(summary.variance.toArray).map(Math.sqrt)
+    val faultsNormed = faultsIn.map { v =>
+      // This is kludge-tastic:
+      val v1 = new BDV(v.toArray)
+      val v2 = (v1 - mean) / stdev
+      Vectors.dense(v2.toArray)
+    }
 
     // Cluster the data into two classes using KMeans
     for (numClusters <- 2 to 40) {
-      println(s"k = $numClusters:")
-      
-      val numIterations = 20
-      val clusters = KMeans.train(faults2, numClusters, numIterations)
+      val numIterations = 100
 
+      val clusters = KMeans.train(faultsNormed, numClusters, numIterations)
       // Evaluate clustering by computing Within Set Sum of Squared Errors
-      val WSSSE = clusters.computeCost(faults2)
-      println("Within Set Sum of Squared Errors = " + WSSSE)
+      val WSSSE = clusters.computeCost(faultsNormed)
+      println(s"Non-PCA, k = $numClusters: WSSSE = $WSSSE")
+      /*
       for (center <- clusters.clusterCenters) {
         println(s"$center")
       }
+       */
     }
 
+    for (dims <- 4 to 16) {
+      // Attempt PCA
+      val pca = new PCA(dims).fit(faultsNormed)
+      val pcaMtx = pca.pc
+      //println(s"PCA matrix: $pcaMtx")
+      val faults2 = pca.transform(faultsNormed)
+
+      for (numClusters <- 2 to 40) {
+        val numIterations = 100
+
+        val clusters = KMeans.train(faults2, numClusters, numIterations)
+        // Evaluate clustering by computing Within Set Sum of Squared Errors
+        val WSSSE = clusters.computeCost(faults2)
+        println(s"PCA ($dims dimensions), k = $numClusters: WSSSE = $WSSSE")
+      }
+    }
+
+    /*
     for (k <- 2 to 10) {
       println(s"k = $k:")
       // Cluster the data into two classes using GaussianMixture
@@ -73,71 +102,22 @@ object UnsupervisedLearning {
           (gmm.weights(i), gmm.gaussians(i).mu, gmm.gaussians(i).sigma))
       }
     }
+     */
     
-    /*
-    val logFile = "kmeans_data.txt"
-
-    val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
-    kmeans(sc)
-    gaussianMixtures(sc)*/
     sc.stop()
   }
 
-  // Major TODO: This ignores the labels.
-  /** Read all the data from the "Steel Faults" data set. */
-  def readSteelFaults(sc : SparkContext) : RDD[Vector] = {
+  /** Read all the data from the "Steel Faults" data set, returning
+    * an RDD of (inputs, outputs). */
+  def readSteelFaults(sc : SparkContext) : RDD[(Vector, Vector)] = {
     // Load and parse the data
-    val fname = "Faults.NNA"
-    val data = sc.textFile(fname)
-    val parsedData = data.map(
-      s => Vectors.dense(s.split('\t').map(_.toDouble))).cache()
-
-    val rows = parsedData.count()
+    val fname = "/home/hodapp/source/cs7641_assignment3/Faults.NNA"
+    val data = sc.textFile(fname).map { str =>
+      val vals = str.split('\t').map(_.toDouble)
+      (Vectors.dense(vals.slice(0, 27)), Vectors.dense(vals.slice(27, 34)))
+    }.cache()
+    val rows = data.count()
     println(s"Read $rows rows from $fname.")
-
-    parsedData
-  }
-
-  def kmeans(sc: SparkContext) {
-
-    // Load and parse the data
-    val data = sc.textFile("kmeans_data.txt")
-    val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
-
-    // Cluster the data into two classes using KMeans
-    val numClusters = 2
-    val numIterations = 20
-    val clusters = KMeans.train(parsedData, numClusters, numIterations)
-
-    // Evaluate clustering by computing Within Set Sum of Squared Errors
-    val WSSSE = clusters.computeCost(parsedData)
-      println("Within Set Sum of Squared Errors = " + WSSSE)
-
-    // Save and load model
-    //clusters.save(sc, "myModelPath")
-    //val sameModel = KMeansModel.load(sc, "myModelPath")
-  }
-
-  def gaussianMixtures(sc: SparkContext) {
-
-    // Load and parse the data
-    val data = sc.textFile("gmm_data.txt")
-    val parsedData = data.map(s => Vectors.dense(s.trim.split(' ').map(_.toDouble))).cache()
-
-    // Cluster the data into two classes using GaussianMixture
-    val gmm = new GaussianMixture().setK(2).run(parsedData)
-
-    // Save and load model
-    //gmm.save(sc, "myGMMModel")
-    //val sameModel = GaussianMixtureModel.load(sc, "myGMMModel")
-
-    // output parameters of max-likelihood model
-    for (i <- 0 until gmm.k) {
-      println("weight=%f\nmu=%s\nsigma=\n%s\n" format
-        (gmm.weights(i), gmm.gaussians(i).mu, gmm.gaussians(i).sigma))
-    }
+    data
   }
 }

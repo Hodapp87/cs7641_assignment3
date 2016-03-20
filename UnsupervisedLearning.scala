3e4fb8967e0fc475b7a80192c364a192f81c3b09
Chris Hodapp <Hodapp87@gmail.com>
2016-03-20 11:24:07 -0400

    More speculative code for k-means

--- UnsupervisedLearning.scala
+++ UnsupervisedLearning.scala
@@ -6,27 +6,26 @@
 // To build without formatted logs (for piping or M-x compile):
 // sbt -Dsbt.log.noformat=true compile
 
+// Running spark-shell with this:
+// spark-shell --driver-class-path /home/hodapp/source/cs7641_assignment3/target/scala-2.10/classes
+
 package cs7641
 
+// Local dependencies:
 import cs7641.Utils._
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.{RDD, DoubleRDDFunctions}
 
 import org.apache.spark.mllib.linalg.{Vectors, Vector}
 import org.apache.spark.mllib.clustering._
 import org.apache.spark.mllib.feature.PCA
-
 import org.apache.spark.mllib.stat.{MultivariateStatisticalSummary, Statistics}
 
 import breeze.linalg.{DenseVector => BDV}
-
 import org.apache.log4j.{Level, Logger}
-
-
-//import com.github.tototoshi.csv._
 
 object UnsupervisedLearning {
 
@@ -49,24 +48,40 @@ object UnsupervisedLearning {
     val faultsNormed = normalize(faultsIn)
 
     // Cluster the data into two classes using KMeans
-    for (numClusters <- 2 to 40) {
+    for (numClusters <- List(5, 10, 20, 40, 80)) {
 
       // Train K-Means & output some basic information:
       val numIterations = 100
       val model : KMeansModel = KMeans.train(faultsNormed, numClusters, numIterations)
-      val WSSSE = clusters.computeCost(faultsNormed)
+      val WSSSE = model.computeCost(faultsNormed)
       println(s"k = $numClusters: WSSSE = $WSSSE")
 
-      // Determine which instances belong to which cluster:
+      // Determine a cluster index for each instance:
       val clusters : RDD[Int] = model.predict(faultsNormed)
 
+      // And then group these together with the class labels:
+      val classes : Array[(Int, RDD[Vector])] =
+        clusters.zip(faultsOut).groupBy(_._1).collect.map {
+          // We have at this stage (Int, Iterable[(Int, Vector)]),
+          // which is a little clumsy and redundant.  We don't need
+          // the IDs inside the Iterable, and we also want to turn it
+          // to an RDD (hence the .collect above - we cannot nest
+          // RDDs).
+          case (id, iter) => (id, sc.parallelize(iter.map(_._2).toSeq))
+        }
+
+      classes.foreach { case(id,v) =>
+        println("%s, count %d, %s" format (id, v.count, Statistics.colStats(v).mean))
+      }
+
       // Compute & output a histogram of these:
+      /*
       val hist = clusters.countByValue()
       val classes = hist.keys.toList.sorted
       classes.foreach { k =>
         val count = hist(k)
         println(s"Class $k, $count elements")
-      }
+      }*/
       /*
       for (center <- clusters.clusterCenters) {
         println(s"$center")
@@ -105,7 +120,6 @@ object UnsupervisedLearning {
   /** Read all the data from the "Steel Faults" data set, returning
     * an RDD of (inputs, outputs). */
   def readSteelFaults(sc : SparkContext) : RDD[(Vector, Vector)] = {
-    // Load and parse the data
     val fname = "/home/hodapp/source/cs7641_assignment3/Faults.NNA"
     val data = sc.textFile(fname).map { str =>
       val vals = str.split('\t').map(_.toDouble)
